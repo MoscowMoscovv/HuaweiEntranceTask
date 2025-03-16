ad290b9470f584df9e8e60621cbd1ef1753a666e
MoscowMoscovv <t.yan2006@yandex.ru>
2025-03-17 00:06:17 +0300

    1)added TimeSlot struct, refactored request struct based on it
    2) added bool intersect_by_bytes method to Request struct
    3) For planed delays on server being possible refactored ThreadPool constructor so threads queues now receive functions and not Requests
    4) Made new ThreadPool field - queues_last_reqs_timeovers and refactored requests_in_queues to all_write_requests... and since we don't need info about reed requests if they aren't last in queue and even if they are - we care only about their timeovers - created queues_last_reqs_timeovers.
    5) Refactored add_request()
    6) Wrote optimal_thread_to_insert main mechanism
    7) Wrote get_best_time_for_thread

--- main.cpp
+++ main.cpp
@@ -4,120 +4,193 @@
 #include <functional>
 #include <mutex>
 #include <queue>
-#include <ranges>
 #include <thread>
 #include <vector>
 #include <string>
 #include <chrono>
 #include <ctime>
+#include <algorithm>
 
 
-struct Request{
-    int8_t id;
-    int8_t thread;
+struct TimeSlot {
+    std::chrono::steady_clock::time_point start;
+    std::chrono::steady_clock::time_point over;
+    std::chrono::milliseconds latency;
+
+    bool operator<(const TimeSlot &other) const {
+        return start < other.start;
+    }
+
+    [[nodiscard]] bool intersects(const TimeSlot &other) const {
+        return (start < other.over) && (over > other.start);
+    }
+};
+
+struct Request {
     std::chrono::steady_clock::time_point timestamp;
-    std::chrono::steady_clock::time_point start_time;
     unsigned int size;
     unsigned int address;
     std::string type;
-    std::chrono::steady_clock::time_point over_time;
+
+    TimeSlot timeSlot;
+
+    //standard comparator for sorted vector
+    bool operator<(const Request &other) const {
+        return timeSlot < other.timeSlot;
+    }
+
+    [[nodiscard]] bool intersects_by_bytes(const Request &other) const;
+
 };
+
+bool Request::intersects_by_bytes(const Request &other) const {
+    return (address < (other.address + other.size) && (address + size) > other.address);
+}
 
 
 class ThreadPool {
 
     std::vector<std::jthread> threads;
-    std::vector<std::queue<Request>> queues_by_threads;
+    std::vector<std::queue<std::function<void()>>> queues_by_threads;
     std::mutex m_mtx;
     std::condition_variable_any m_cv;
-    std::vector<Request> requests_in_queues;
 
-    ThreadPool(int8_t N){
+    std::vector<std::chrono::steady_clock::time_point> queues_last_reqs_timeovers;
+    // has to contain whole request so we can find write reqs that intersect given one by bytes
+    std::vector<Request> all_write_requests_in_every_queue;
+
+    explicit ThreadPool(std::uint8_t N) {
         for (size_t i = 0; i < N; ++i) {
-            threads.emplace_back([this, i](std::stop_token stok){
+            threads.emplace_back([this, i](std::stop_token stok) {
                 while (true) {
                     std::unique_lock<std::mutex> lk(m_mtx);
 
                     /* Wait for either a task to be added, or a stop to be requested */
-                    m_cv.wait(lk, stok, [this,i]() { return !queues_by_threads[i].empty(); });
+                    m_cv.wait(lk, stok, [this, i]() { return !queues_by_threads[i].empty(); });
                     if (stok.stop_requested() && queues_by_threads[i].empty()) {
                         return;
                     }
 
-                    Request request = queues_by_threads[i].front();
+                    std::function<void()> fn = queues_by_threads[i].front();
                     queues_by_threads[i].pop();
                     lk.unlock();
-                    std::this_thread::sleep_for(request.over_time-request.start_time);
+                    //
+                    fn();
+
                 }
             });
 
-            }
         }
+    }
 
-    std::vector<Request> requests_in_queues_that_interferes_with_req (Request req){
-        std::vector<Request> intersects;
-        for(const auto written : requests_in_queues){
-            bool start_in_written = (req.address >= written.address && req.address <= written.size);
-            bool end_in_written = ((req.address + req.size) >= written.address && (req.address + req.size) <= written.size);
-            if(start_in_written || end_in_written){
-                /* todo tommorrow(today): check if it's really bad idea to return list of whole requests rather than just array of [thread_id, start_time, over_time]*/
-                intersects.push_back(req);
+
+    // Iterate by write requests in all queues and return timeslots of those that intersects given req by bytes
+    std::vector<TimeSlot> timeslots_of_write_requests_in_queues_that_interferes_with_req_by_bytes(Request req) {
+        std::vector<TimeSlot> intersects;
+        for (const auto &written_write_req: all_write_requests_in_every_queue) {
+            if (req.intersects_by_bytes(written_write_req)) {
+                intersects.push_back(written_write_req.timeSlot);
             }
         }
         return intersects;
     }
 
-    void add_request(int8_t thread, Request request,std::chrono::steady_clock::time_point timestamp){
-        //std::chrono::steady_clock::time_point timestamp = std::chrono::steady_clock::now();
-        std::queue<Request> queue = queues_by_threads.at(thread);
-        int8_t base_latency = 1 + static_cast<int8_t>(request.type == "READ");
-        std::chrono::microseconds latency = std::chrono::microseconds(request.size*base_latency);
-        if (queue.empty() || queue.front().over_time < timestamp){
-            request.start_time = request.timestamp;
-            request.over_time = request.timestamp + latency;
-            requests_in_queues.push_back(request);
-            queue.push(request);
+    void add_request(Request request) {
+
+
+
+        // calculating info out of request's incomes:
+        std::uint8_t base_latency = (request.type == "READ") ? 2 : 1;
+        request.timeSlot.latency = std::chrono::milliseconds(base_latency * request.size);
+        auto latency = request.timeSlot.latency;
+        //calculating best place to put request
+        std::pair<uint8_t, std::chrono::microseconds> optimal_thread_and_time_to_wait = optimal_thread_to_insert(
+                request);
+
+        // parsing output
+        auto thread_id = optimal_thread_and_time_to_wait.first;
+        auto time_to_wait = optimal_thread_and_time_to_wait.second;
+        auto queue = queues_by_threads.at(thread_id);
+
+        //if thread need to wait
+        if (std::chrono::microseconds(0) != time_to_wait) {
+            //put delay
+            queue.emplace([time_to_wait]() { std::this_thread::sleep_for(time_to_wait); });
+            queue.emplace([latency]() { std::this_thread::sleep_for(latency); });
+        } else {
+            queue.emplace([latency]() { std::this_thread::sleep_for(latency); });
         }
-        else{
-            request.start_time = queue.front().over_time;
-            request.over_time = request.start_time + latency;
-            requests_in_queues.push_back(request);
-            queue.push(request);
+        if (request.type == "WRITE") {
+            const auto place = std::lower_bound(all_write_requests_in_every_queue.begin(),
+                                                all_write_requests_in_every_queue.end(), request);
+            all_write_requests_in_every_queue.insert(place, request);
         }
+        queues_last_reqs_timeovers.at(thread_id) = request.timeSlot.over;
     }
 
-    int8_t optimal_thread_to_insert(std::vector<int8_t> ids, std::chrono::microseconds latency){
+    std::pair<uint8_t, std::chrono::microseconds> optimal_thread_to_insert(Request &req) {
+
         std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
         std::vector<std::chrono::steady_clock::time_point> best_times(queues_by_threads.size());
-        std::queue<Request> queue;
-        Request req;
 
+        std::vector<TimeSlot> interfering_reqs_timeslots = timeslots_of_write_requests_in_queues_that_interferes_with_req_by_bytes(
+                req);
 
-        for (int8_t queue_id ; queue_id < queues_by_threads.size(); ++queue_id){
-            queue = queues_by_threads.at(queue_id);
-            if (queue.empty()){
-                best_times.at(queue_id) = std::chrono::steady_clock::now();
-            }
-            /* todo tommorrow(today): make it work as I drew on whiteboard */
-            if (queue.)
+        for (size_t queue_id = 0; queue_id != queues_by_threads.size() - 1; ++queue_id) {
+            best_times.push_back(get_best_time_for_thread(queue_id, interfering_reqs_timeslots, req.timeSlot.latency));
         }
+
+        auto best_element_iterator = std::min_element(best_times.begin(), best_times.end());
+        uint8_t thread_id = std::distance(best_times.begin(), best_element_iterator);
+        std::chrono::microseconds time_difference = std::chrono::duration_cast<std::chrono::microseconds>(
+                *best_element_iterator - now
+        );
+        return std::pair<uint8_t, std::chrono::microseconds>{thread_id, time_difference};
+
 
     }
 
-    bool timestamp_intersects_interfering_requests(std::chrono::steady_clock::time_point timestamp, Request req){
-        std::vector<Request> interfering_reqs = requests_in_queues_that_interferes_with_req(req);
-        for (const auto interfering_req : interfering_reqs){
-            if (timestamp>=req.start_time && timestamp<=req.over_time){
-                return true;
-            }
-        }
-        return false;
-    }
+    std::chrono::steady_clock::time_point
+    get_best_time_for_thread(size_t queue_id, std::vector<TimeSlot> interfering_reqs_timeslots,
+                             std::chrono::microseconds latency) {
+        auto now = std::chrono::steady_clock::now();
+        auto max_server_inactivity_time = std::chrono::microseconds(7);
+        auto queues_last_req_timeover = queues_last_reqs_timeovers.at(queue_id);
+        TimeSlot hypothetical_place_to_put_request{std::chrono::steady_clock::time_point::max()};
 
+        if (queues_last_req_timeover < now) {
+            hypothetical_place_to_put_request = TimeSlot{now, now + latency};
+        } else {
+            hypothetical_place_to_put_request = TimeSlot{queues_last_req_timeover, queues_last_req_timeover + latency};
+
+        }
+
+
+        for (auto &interfering_reqs_timeslot: interfering_reqs_timeslots) {
+            // if this process out of our interests range
+            if (interfering_reqs_timeslot.start > queues_last_req_timeover + latency + max_server_inactivity_time) {
+                //if it is - we don't need to even look at next one
+                return hypothetical_place_to_put_request.start;
+            }
+            //if process intersects hypothetical place to put request
+            if (interfering_reqs_timeslot.intersects(hypothetical_place_to_put_request)) {
+                //if when process is over we can insert request, and it won't be inefficient
+                if (interfering_reqs_timeslot.over - queues_last_req_timeover < max_server_inactivity_time) {
+                    hypothetical_place_to_put_request = TimeSlot{interfering_reqs_timeslot.over,
+                                                                 interfering_reqs_timeslot.over + latency};
+                }
+                    //if it will be we don't need to even look at next one
+                else {
+                    return std::chrono::steady_clock::time_point::max();
+                }
+            }
+            return hypothetical_place_to_put_request.start;
+        }
+    }
 };
 
-int main(){
 
+int main() {
 
 
 }
